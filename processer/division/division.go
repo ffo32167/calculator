e2ab7fb43cb8bf99387c45466d8106cd8cc6455f
ff032167 <dmi32167@yandex.ru>
2021-09-07 16:34:33 +0300

    исправил алиас

--- processer/division/division.go
+++ processer/division/division.go
@@ -17,9 +17,9 @@ func round(in float64, precision float64) float64 {
 	return math.Round(in*math.Pow(10, precision)) / math.Pow(10, precision)
 }
 
-func (p division) Calculate(first float64, last float64) (float64, error) {
+func (d division) Calculate(first float64, last float64) (float64, error) {
 	if last == 0 {
 		return 0, errors.New("Calculate division error, last number can't be 0")
 	}
-	return round(first/last, p.precision), nil
+	return round(first/last, d.precision), nil
 }
